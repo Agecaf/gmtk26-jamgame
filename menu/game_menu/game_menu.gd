7164e511a7db781a837a06f03317a0c8141e5f0e
Agecaf <ageof.cafeine@gmail.com>
2026-07-23 23:00:41 +0100

    Connected Post Level Menu.

--- menu/game_menu/game_menu.gd
+++ menu/game_menu/game_menu.gd
@@ -9,5 +9,14 @@ func _ready_deferred() -> void:
 
 # Check if we need to start a new level, or if we're just returning from pause
 func start() -> void:
+	# New level
 	if Game.menu.previous_menu_name == &"PreLevelMenu":
 		Game.container.load_level()
+	
+	# Retry
+	if Game.menu.previous_menu_name == &"PostLevelMenu":
+		Game.container.load_level()
+	
+	# Unpause
+	if Game.menu.previous_menu_name == &"PauseMenu":
+		pass
