9b16cbb0762f1a0e891aa4bff3e0f6c685227c10
Zerrrook <1221manuelluiz0514@gmail.com>
2026-07-24 15:28:52 +0800

    refactor: simplified the hit system

--- game/player/scripts/hurtbox_component.gd
+++
@@ -1,8 +0,0 @@
-class_name HurtComponent extends Area2D
-
-signal hit(hit_data : int)
-
-func _on_area_entered(area: Area2D) -> void:
-	if area.has_method("get_hit_data"):
-		var data : int = area.get_hit_data()
-		hit.emit(data)
--- game/traps/hitbox_component.gd
+++ game/traps/hitbox_component.gd
@@ -1,13 +1,3 @@
 # Attach this scene to any projectile/object that hurts the player
 # then add a collision to it, shape it to anything you want
-
 class_name HitboxComponent extends Area2D
-
-@export var damage : int = 1
-
-# if you want to add knockback in the future
-# have the function return a dictionary and include it with damage
-func get_hit_data() -> int:
-	# var direction = (target_position - global_position).normalized() <- for Knockback 
-	
-	return damage
