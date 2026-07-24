8ffb581d8e9bec945e4a3cff915823ee4f1d7762
Agecaf <ageof.cafeine@gmail.com>
2026-07-24 16:51:00 +0100

    Can now change font.

--- menu/settings_menu/settings_menu.gd
+++ menu/settings_menu/settings_menu.gd
@@ -3,6 +3,7 @@ extends Control
 
 @onready var return_button: Button = %Return
 @onready var music_volume: HSlider = %MusicVolume
+@onready var font_button: Button = %FontButton
 @onready var sfx_volume: HSlider = %SFXVolume
 
 # Initialization
@@ -11,6 +12,7 @@ func _ready() -> void:
 	return_button.pressed.connect(return_to_previous)
 	sfx_volume.value_changed.connect(sfx_changed)
 	music_volume.value_changed.connect(music_changed)
+	font_button.pressed.connect(change_font)
 	sfx_volume.value = 80
 	music_volume.value = 80
 	sfx_changed(80)
@@ -34,3 +36,18 @@ func music_changed(value: float) -> void:
 func sfx_changed(value: float) -> void:
 	AudioServer.set_bus_mute(2, value <= 2.0)
 	AudioServer.set_bus_volume_linear(2, value / 200.0)
+
+
+# Change font
+var which_font: bool = true
+var pixel_font = preload("res://assets/other/font/Jacquard24-Regular.ttf")
+var other_font = preload("res://assets/other/font/Rosarivo-Regular.ttf")
+func change_font() -> void:
+	which_font = not which_font
+	if which_font:
+		Game.menu.theme.default_font = pixel_font
+		Game.menu.theme.default_font_size = 43
+	else:
+		Game.menu.theme.default_font = other_font
+		Game.menu.theme.default_font_size = 40
+	pass
