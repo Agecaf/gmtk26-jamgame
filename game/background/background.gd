f7e3fe9125e79dd31fbf7dbd19ba508d9cb28229
Agecaf <ageof.cafeine@gmail.com>
2026-07-25 18:11:51 +0100

    Added horizon.

--- game/background/background.gd
+++ game/background/background.gd
@@ -2,16 +2,18 @@ extends Node2D
 
 @export var number_of_small_clouds = 20
 @onready var sky: Sprite2D = %Sky
+@onready var sun: AnimatedSprite2D = %Sun
+@onready var horizon: Sprite2D = %Horizon
 var small_cloud_frames = preload("res://assets/art/background/small_clouds.tres")
 
 var time: int = 0
 
 var bg_colors: Array[Color] = [
 	Color("#000000"),
+	Color("#000000"),
+	Color("#452729"),
 	Color("#452729"),
 	Color("#702c1f"),
-	Color("#702c1f"),
-	Color("#4f5878"),
 ]
 
 func _ready() -> void:
@@ -38,13 +40,27 @@ func _process(delta: float) -> void:
 	for sc in %SmallClouds.get_children():
 		sc.position.x += (100.0 + sc.position.y) * delta * 0.02
 		if sc.position.x > 2000: sc.position.x = -200
+	
+	# Horizon
+	if Game.countdown.is_stopped():
+		horizon.modulate = Color.DARK_RED
+	else:
+		var l = 1.0 - Game.countdown.time_left / 60.0
+		horizon.modulate = Color(l, l, l, 1.0)
+	
+	
+	# Sun
+	if Game.countdown.is_stopped():
+		sun.position.y = 450
+	else:
+		sun.position.y = lerpf(450, 700, Game.countdown.time_left / 60.0)
 
 
 func on_tick(seconds_left: int) -> void:
-	if seconds_left < 5: time = 4
-	elif seconds_left < 10: time = 3
-	elif seconds_left < 20: time = 2
-	elif seconds_left < 30: time = 1
+	if seconds_left < 10: time = 4
+	elif seconds_left < 20: time = 3
+	elif seconds_left < 30: time = 2
+	elif seconds_left < 45: time = 1
 	else: time = 0
 	update()
 
