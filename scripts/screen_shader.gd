def933d6867eebb801c7f979be3cbd769c4f8728
Agecaf <ageof.cafeine@gmail.com>
2026-07-25 15:46:01 +0100

    Coffin ends the level.

--- scripts/screen_shader.gd
+++ scripts/screen_shader.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
 func _process(_delta: float) -> void:
-	(material as ShaderMaterial).set_shader_parameter(&"time_left", Game.countdown.time_left)
+	if not Game.countdown.is_stopped():
+		(material as ShaderMaterial).set_shader_parameter(&"time_left", Game.countdown.time_left)
 	
