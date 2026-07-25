8e9ca3c7c087bd1553b7adad9b940210e6b05696
Parrotwise <parrotwisearts@gmail.com>
2026-07-25 13:17:22 +0300

    Disable crouch running, disable movement during crouching, allow seamless crouching on landing after jumps/glides

--- game/player/player_state.gd
+++ game/player/player_state.gd
@@ -67,7 +67,7 @@ func _on_player_physics_process(delta: float) -> void:
 				player.change_state(Player.State.JUMPING)
 				player.pocketwatch_close()
 			
-			if Input.is_action_just_pressed(&'crouch'):
+			if Input.is_action_pressed(&'crouch'):
 				player.change_state(Player.State.CROUCHING)
 				player.pocketwatch_close()
 			
@@ -79,8 +79,13 @@ func _on_player_physics_process(delta: float) -> void:
 			if Input.is_action_just_pressed(&'jump') and total_air_time <= player.coyote_time:
 				player.change_state(Player.State.JUMPING)
 			
+			## Crouch running is disabled
+			# if Input.is_action_just_pressed(&'crouch'):
+			# 	player.change_state(Player.State.CROUCHING_RUN)
+			
+			# Transition to crouching instead
 			if Input.is_action_just_pressed(&'crouch'):
-				player.change_state(Player.State.CROUCHING_RUN)
+				player.change_state(Player.State.CROUCHING)
 			
 			if not player.velocity.x:
 				player.change_state(Player.State.IDLE)
@@ -89,8 +94,9 @@ func _on_player_physics_process(delta: float) -> void:
 			if Input.is_action_just_released(&'crouch'):
 				player.change_state(Player.State.IDLE)
 			
-			if player.velocity.x:
-				player.change_state(Player.State.CROUCHING_RUN)
+			## Crouch running is disabled
+			# if player.velocity.x:
+			# 	player.change_state(Player.State.CROUCHING_RUN)
 		
 		Player.State.CROUCHING_RUN:
 			if Input.is_action_just_released(&'crouch'):
