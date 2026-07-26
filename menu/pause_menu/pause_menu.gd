5d64e10a24ac3777bdcbfdf23b52a2a06308a6e3
Agecaf <ageof.cafeine@gmail.com>
2026-07-26 14:21:30 +0100

    Crossbow level.

--- menu/pause_menu/pause_menu.gd
+++ menu/pause_menu/pause_menu.gd
@@ -18,6 +18,9 @@ func start() -> void:
 	# Pause
 	get_tree().paused = true
 	Game.audio.game_music.stream_paused = true
+	
+	if Game.menu.previous_menu_name != &"SettingsMenu":
+		Game.audio.prelevel_music.play()
 
 # End the pause
 func end() -> void:
@@ -28,11 +31,13 @@ func end() -> void:
 		Game.game_end.emit()
 		Game.audio.game_music.stop()
 		Game.countdown.stop()
+		Game.audio.prelevel_music.stop()
 	
 	# Returning to game
 	if Game.menu.next_menu_name == &"GameMenu":
 		get_tree().paused = false
 		Game.audio.game_music.stream_paused = false
+		Game.audio.prelevel_music.stop()
 
 func _process(_delta: float) -> void:
 	if Game.menu.current_menu.name == &"GameMenu":
